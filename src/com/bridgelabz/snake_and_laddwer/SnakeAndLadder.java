7d246d0f6b941f13e09dbfbc6f305fd7316d357a
Aditya Bahakar <adityabahakar99@gmail.com>
2022-03-14 19:20:07 +0530

    The Player rolls the die

--- src/com/bridgelabz/snake_and_laddwer/SnakeAndLadder.java
+++ src/com/bridgelabz/snake_and_laddwer/SnakeAndLadder.java
@@ -3,6 +3,8 @@ package com.bridgelabz.snake_and_laddwer;
 public class SnakeAndLadder {
     public static void main(String[] Args) {
         int start = 0;
-        System.out.println("Start Position:" +start);
+        System.out.println("The start position :"+start);
+        int roll = (int) (Math.floor(Math.random() *10)%6+1);
+        System.out.println("After rolling the dice : "+ roll);
     }
 }
